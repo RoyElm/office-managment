310f245d7ae7178b4c9bb3bdc7c71e1d604de7f4
RoyElm <roye456@justt.ai>
2025-04-11 17:30:48 +0300

    Add cache control meta tags to prevent CDN caching

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,6 +24,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta httpEquiv="cache-control" content="no-cache, no-store, must-revalidate" />
+        <meta httpEquiv="expires" content="0" />
+        <meta httpEquiv="pragma" content="no-cache" />
+        <link rel="icon" href="/office-managment/favicon.ico" type="image/x-icon" sizes="16x16" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
